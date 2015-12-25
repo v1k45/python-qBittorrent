17cab926193ef870079c5e887fee4b385e8a64d8
v1k45 <v1k45@users.noreply.github.com>
2015-12-25 18:54:20 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup, find_packages
+import qbittorrent
+
+install_requires = open('requirements.txt').read().splitlines()
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='python-qbittorrent',
+    description='Python wrapper for qBittorrent >3.1.x',
+    version=qbittorrent.__version__,
+    long_description=read('README.rst'),
+    license='The MIT License',
+    platforms=['OS Independent'],
+    keywords='torrent, qBittorent, API, wrapper',
+    author='Vikas Yadav',
+    author_email='v1k45x@gmail.com',
+    url="https://github.com/v1k45/python-qbittorrent",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console'
+    ]
+)
